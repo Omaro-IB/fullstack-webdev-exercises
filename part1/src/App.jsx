23a9ef43c8149d36367b075488cc20aadab6c76d
Omar Ibrahim <omarom.ibra@outlook.com>
2024-05-28 16:49:34 +0300

    created react app for p1

---
+++ part1/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Hello = () => {  return (    <div>      <p>Hello world</p>    </div>  )}
+
+const App = () => {
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello />
+            <Hello />
+            <Hello />
+        </div>
+    )
+}
+
+export default App
