86cab2af2b9821be80c2b5e6e5a128b6824679c3
Omar Ibrahim <omarom.ibra@outlook.com>
2024-05-29 01:47:07 +0300

    exercises 1.1-1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.course}</h1>
+        </div>
+    )
+}
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+const Part = (props) => {
+    return (
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+            {props.partName} {props.numExercises}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+}
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part partName={props.part1} numExercises={props.exercises1}/>
+            <Part partName={props.part2} numExercises={props.exercises2}/>
+            <Part partName={props.part3} numExercises={props.exercises3}/>
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p>Number of exercises {props.totalExercises}</p>
+    )
+}
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+    return (
+        <div>
+            <Header course={course}/>
+            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+            <Total totalExercises={exercises1 + exercises2 + exercises3}/>
+        </div>
+    )
 }
 
 export default App
