fedd74f741330b281c878015138cc4e4cca3814f
Omar Ibrahim <omarom.ibra@outlook.com>
2024-06-08 07:22:58 +0400

    exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [goodCount, setGoodCount] = useState(0)
+  const [neutralCount, setNeutralCount] = useState(0)
+  const [badCount, setBadCount] = useState(0)
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <h1>Give Feedback:</h1>
+          <div style={{flexDirection: "row"}}>
+              <button onClick={() => setGoodCount(goodCount+1)}>Good :)</button>
+              <button onClick={() => setNeutralCount(neutralCount+1)}>Neutral :|</button>
+              <button onClick={() => setBadCount(badCount+1)}>Bad :(</button>
+          </div>
+          <h1>Statistics:</h1>
+          <div>
+              <p>Good: {goodCount}</p>
+              <p>Neutral: {neutralCount}</p>
+              <p>Bad: {badCount}</p>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
