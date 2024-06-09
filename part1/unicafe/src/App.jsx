c1ba9be1c8356b6df68742311e1841e7bd551834
Omar Ibrahim <omarom.ibra@outlook.com>
2024-06-09 08:56:38 +0400

    exercises 1.7-1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,24 +1,53 @@
 import { useState } from 'react'
 
-function App() {
-  const [goodCount, setGoodCount] = useState(0)
-  const [neutralCount, setNeutralCount] = useState(0)
-  const [badCount, setBadCount] = useState(0)
 
-  return (
-      <div>
-          <h1>Give Feedback:</h1>
-          <div style={{flexDirection: "row"}}>
-              <button onClick={() => setGoodCount(goodCount+1)}>Good :)</button>
-              <button onClick={() => setNeutralCount(neutralCount+1)}>Neutral :|</button>
-              <button onClick={() => setBadCount(badCount+1)}>Bad :(</button>
+const StatisticLine = ({text, value}) => {
+    return (<tr><td>{text}</td><td>{value}</td></tr>)
+}
+
+
+const Statistics = ({good, neutral, bad}) => {
+    const total = good + neutral + bad
+    if (total === 0) {
+        return (
+            <p>No feedback given</p>
+        )
+    }
+
+    const avg = ((good - bad) / total * 100).toFixed(0)
+    const pve = (good / total * 100).toFixed(0)
+
+    return (<div>
+        <h1>Statistics:</h1>
+        <table><tbody>
+            <StatisticLine text={"Good"} value={good}></StatisticLine>
+            <StatisticLine text={"Neutral"} value={neutral}></StatisticLine>
+            <StatisticLine text={"Bad"} value={bad}></StatisticLine>
+            <StatisticLine text={"All"} value={total}></StatisticLine>
+            <StatisticLine text={"Average"} value={avg+"%"}></StatisticLine>
+            <StatisticLine text={"Positive"} value={pve+"%"}></StatisticLine>
+        </tbody></table>
+    </div>)
+}
+
+
+const Button = ({text, handleClick}) => {return (<button onClick={handleClick}>{text}</button>)}
+
+
+function App() {
+    const [goodCount, setGoodCount] = useState(0)
+    const [neutralCount, setNeutralCount] = useState(0)
+    const [badCount, setBadCount] = useState(0)
+
+    return (
+        <div>
+            <h1>Give Feedback:</h1>
+            <div style={{flexDirection: "row"}}>
+                <Button handleClick={() => setGoodCount(goodCount + 1)} text="Good :)"></Button>
+                <Button handleClick={() => setNeutralCount(neutralCount+1)} text="Neutral :|"></Button>
+                <Button handleClick={() => setBadCount(badCount+1)} text="Bad :("></Button>
           </div>
-          <h1>Statistics:</h1>
-          <div>
-              <p>Good: {goodCount}</p>
-              <p>Neutral: {neutralCount}</p>
-              <p>Bad: {badCount}</p>
-          </div>
+            <Statistics good={goodCount} neutral={neutralCount} bad={badCount} />
       </div>
   )
 }
